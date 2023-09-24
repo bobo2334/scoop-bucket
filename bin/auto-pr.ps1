f8948094c8919b42bfbf5c4d2c74c7a402f3aa8b
Bug-Proof <bobojiangg@gmail.com>
2023-09-24 15:52:51 +0800

    chore(ci): 配置 Github Actions

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "<bobo2334>/<scoop-bucket>:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
